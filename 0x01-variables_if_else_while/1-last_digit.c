311398bd1e6e78304a71286f8bc74e1b0cfe53bf
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-05 19:55:00 +0000

    Fix output error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,6 +25,6 @@ int main(void)
 	else if (mod == 0)
 		printf("Last digit of %d is %d and is 0\n", n, mod);
 	else if (mod < 6 && mod != 0)
-		printf("Last digit of %d is %d less than 6 and not 0", n, mod);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mod);
 	return (0);
 }
