98a3be270527f64393e8d3d48241ad7c6c15f36d
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-06 10:49:12 +0000

    Write script that prints numbers 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main function prints all single
+ * digit numbers of base 10, starting from
+ * 0 followed by a new line.
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n > 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
