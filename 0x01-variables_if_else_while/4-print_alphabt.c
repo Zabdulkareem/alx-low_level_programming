6fbc433c85528f03a76e4db15c22a9ba72a90144
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-06 10:29:34 +0000

    Add script that prints alphabets except 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main function prints out lowercase
+ * alphabets except q and e.
+ * Return: returns 0 on success.
+ */
+int main(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar (a);
+			a++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
