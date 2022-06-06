5f5318ab059a2ac4667ab6a5a97b82f4aa200569
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-06 09:53:32 +0000

    Write program that prints upper and lower case letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,11 +3,19 @@
 /**
  * main - Entry point
  *
- * Description:
+ * Description: main function prints alphabets in lowercase
+ * then uppercase using the putchar function only.
+ * It can only be used in the program thrice.
  * Return: returns 0 on success
  */
 int main(void)
 {
+	int x, y;
 
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (y = 'A'; y <= 'Z'; y++)
+		putchar(y);
+	putchar('\n');
 	return (0);
 }
