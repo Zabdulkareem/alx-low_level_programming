7b81db18f474da670032dd6349ab2dd1b2150f41
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-07 03:27:14 +0000

    Write script that prints possible combos of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main function prints all possible
+ * combinations of single-digit numbers in
+ * ascending order, separated by '-'
+ * using only putchar function
+ * Return: return 0 always on success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n')
+	return (0);
+}
