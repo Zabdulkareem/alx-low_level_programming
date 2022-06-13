7f9cdda295e6fdf955c23478c57365ee27a5a0a5
Zainab Abdulkareem <zabdulkareem@gmail.com>
2022-06-13 21:10:48 +0000

    Write function that takes a pointer to an int as argument

---
+++ 0x05-printers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter
+ * and updates the value it points to to 98
+ * @n: integer arguement that is a pointer
+ * void: returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-printers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+In this directory I am practicing how to use pointers and arrays in C programming
+
