44d6f3191e813ee162b3ec81f9902b71df615f6a
Zainab <zabdulkareem01@gmail.com>
2023-07-17 10:53:57 +0100

    Fix betty coding style error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Program prints size of various types on 
+ * Description: Program prints size of various types on
  * the computer it is compiled and run on.
  *
  * Return: Always zero (Success)
