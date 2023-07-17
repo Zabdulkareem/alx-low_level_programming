34a6e70920df3532ebfe703b7ed4c69689cffff2
Zainab <zabdulkareem01@gmail.com>
2023-07-17 11:28:02 +0100

    Write a file that prints to stdout with `write` function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main prints to the stdout without using any functions in Name
+ * section of 'printf' or 'puts'
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	const char sentence[] = "and that piece of art is useful\" -Dora Korp\
+				 ar, 2015-10-19\n";
+	size_t sentence_len = sizeof(sentence) - 1;
+
+	write(1, sentence, sentence_len);
+
+	return (1);
+}
