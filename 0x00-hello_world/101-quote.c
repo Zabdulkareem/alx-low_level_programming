3c154371bd74123168421e514e4f851c4ed54927
Zainab <zabdulkareem01@gmail.com>
2023-07-17 11:31:22 +0100

    Write a file that prints to stdout with `write` function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	const char sentence[] = "and that piece of art is useful\" -Dora Korp\
-				 ar, 2015-10-19\n";
+	const char sentence[] = "and that piece of art is useful\" -Dora Korpar\
+		, 2015-10-19\n";
 	size_t sentence_len = sizeof(sentence) - 1;
 
 	write(1, sentence, sentence_len);
