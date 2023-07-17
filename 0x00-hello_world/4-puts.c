84e544ca3b29f207cf4508b1d9dad332df108421
Zainab <zabdulkareem01@gmail.com>
2023-07-17 09:56:58 +0100

    Add a c file that prints with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main prints "Programming is like building a multilingual
+ * puzzle"
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
