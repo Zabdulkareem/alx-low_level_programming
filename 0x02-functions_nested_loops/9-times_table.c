ddc53566ce4ebd76a2c01da38e2dc3c948ae197d
Zainab <zabdulkareem01@gmail.com>
2023-07-18 22:58:00 +0100

    Add function that prints up to 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - Times table function
+ *
+ * Description: This function prints the times table starting with
+ * zero up to nine times
+ * Return: nothing is returned
+ */
+void times_table(void)
+{
+	int a, b, product;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			product = a * b;
+
+			if (product > 9)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if (b != 1)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+				_putchar(product + '0');
+
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
