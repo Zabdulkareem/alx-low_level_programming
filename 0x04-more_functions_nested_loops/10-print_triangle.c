22d1639fd27a6924ceb875a500d8dc4750fca185
Zainab <zabdulkareem01@gmail.com>
2023-07-23 16:58:04 +0100

    Fix code indents and spacing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,24 +11,24 @@
  */
 void print_triangle(int size)
 {
-        int i, j;
+	int i, j;
 
-        if (size <= 0)
-                _putchar('\n');
-        else
-        {
-                for (i = 1; i <= size; i++)
-                {
-                        for (j = 1; j <= size - 1; j++)
-                        {
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - 1; j++)
+			{
 				_putchar(' ');
 			}
 
-                        for (j = 1; j <= i; j++)
-                        {
-                                _putchar('#');
-                        }
-                        _putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-        }
+	}
 }
