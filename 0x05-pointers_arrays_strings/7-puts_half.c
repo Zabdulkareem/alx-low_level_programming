dfe8ca8f846bc4305da4a19ec9ae830f6b6af2a5
Zainab <zabdulkareem01@gmail.com>
2023-08-02 12:31:24 +0100

    Fix code logic for even length

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,16 +20,19 @@ void puts_half(char *str)
 	}
 
 	/* Find the start of index of second half of string */
-	start_index = len / 2;
 	if (len % 2 == 1)
 	{
-		start_index = (len - 1) / 2;	/* Adjust index for odd len num */
+		start_index = (len - 1) / 2;	/* Adjusts start_index for odd len */
+	}
+	else
+	{
+		start_index = len / 2;	/* Sets start_index for even len */
 	}
 
-	/* Next print the second half of string */
+	/* Print the second half of the string */
 	for (i = start_index; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
-
---
+++ 0x05-pointers_arrays_strings/text.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char *c[] = "Hello Zainab";
+
+	_putchar(*c);
+	return (0);
+}
